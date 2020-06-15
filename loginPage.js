a890c534b1c3ff6e2012be787797ef1d415181b1
FilipKalicanin <filipkalicanin@hotmail.com>
2020-06-15 12:47:18 +0200

    added JS for username and password

--- loginPage.js
+++ loginPage.js
@@ -1,3 +1,11 @@
-function myFunction() {
-   document.getElementById("button").innerHTML = "Thank you!";
+function check(form) {
+   
+   if(form.userid.value == "Filip" && form.password.value == "123") 
+   {
+      window.open("https://www.youtube.com/")
+   }
+   else 
+   {
+      alert("Username or password does not match!")
+   }
 }
